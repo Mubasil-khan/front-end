af861b1692be3a36ff08398d2d6213cdfdba1a90
Mubasil <mubasil.emedstore@gmail.com>
2025-07-29 14:25:57 +0530

    Add Products page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Banner from "./(pages)/Components/MainPage/Banner";
 import Categories from "./(pages)/Components/MainPage/Categories";
+import CenterBanner from "./(pages)/Components/MainPage/CenterBanner";
+import Products from "./(pages)/Components/MainPage/Products";
 
 export default function Home() {
   return (
    <div>
     <Banner />
     <Categories />
+    <Products />
+    <CenterBanner />
    </div>
   );
 }
